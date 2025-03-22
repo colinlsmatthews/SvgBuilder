9a82b839426553c26a2b34046d07b675071093ac
Colin Matthews <25379751+colinlsmatthews@users.noreply.github.com>
2025-03-22 13:39:18 -0400

    add input logic for dimensions

--- Console/Program.cs
+++ Console/Program.cs
@@ -16,21 +16,30 @@ class Program
         string? inputPath = String.Empty;
         string? outputPath = String.Empty;
         string outputDestination = String.Empty;
+        int? inputWidth = null;
+        int? inputHeight = null;
         Exception e = new Exception();
 
-        // Display welcome message and prompt for input
+        // Display welcome message and prompt for file input
         Console.WriteLine("Welcome to SVG Builder!");
         Console.WriteLine("Please enter the path to the JSON specification for your SVG:");
         inputPath = GetInput(InputType.Path);
         Console.WriteLine("Please enter the path to your destination directory:");
         outputPath = GetInput(InputType.Directory);
 
+        // Handle dimension input
+        GetDimChoice(out string sizeChoice);
+        if (sizeChoice == "Y")
+        {
+            GetInputDims(out inputWidth, out inputHeight);
+        }
+
         //inputPath = "C:\\Users\\clsm\\source\\repos\\SvgBuilder\\Core\\input.json";
         //outputPath = "C:\\Users\\clsm\\source\\repos\\SvgBuilder\\Core\\";
 
         // Call svg builder and display results
         Builder svgBuilder = new Builder();
-        if (svgBuilder.Build(inputPath, outputPath, out outputDestination, out e))
+        if (svgBuilder.Build(inputPath, outputPath, inputWidth, inputHeight, out outputDestination, out e))
         {
             Console.ForegroundColor = ConsoleColor.Green;
             Console.WriteLine("\nSVG built successfully!");
@@ -73,6 +82,37 @@ class Program
                 break;
         }
         return input;
+    }
+
+    private static void GetDimChoice(out string sizeChoice)
+    {
+        Console.WriteLine("Enter custom width and height? (Y/N)");
+        sizeChoice = Console.ReadLine().ToUpper();
+        if (sizeChoice != "Y" && sizeChoice != "N")
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Invalid input. Please enter 'Y' or 'N':");
+            Console.ResetColor();
+            GetDimChoice(out sizeChoice);
+        }
+    }
+
+    private static void GetInputDims(out int? inputWidth, out int? inputHeight)
+    {
+        try
+        {
+            Console.WriteLine("Enter width (px):");
+            inputWidth = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter height (px):");
+            inputHeight = int.Parse(Console.ReadLine());
+        }
+        catch (Exception ex)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Invalid input. Please provide valid integers for width and height:");
+            Console.ResetColor();
+            GetInputDims(out inputWidth, out inputHeight);
+        }
     }
 
     private enum InputType
