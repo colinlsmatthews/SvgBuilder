5c78a9a9a9cb38fbb941b964c54ffb2e4d1ce55f
Colin Matthews <25379751+colinlsmatthews@users.noreply.github.com>
2025-03-22 12:08:46 -0400

    establish svg element class

---
+++ Core/SvgElement.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Drawing;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SvgBuilder.Core
+{
+    public class SvgElement
+    {
+        public string? Id { get; set; }
+        public Tuple<int, int> Min { get; set; }
+        public Tuple<int, int> Max { get; set; }
+        public Color Color { get; set; }
+        public string Type { get; set; }
+
+        public SvgElement(Tuple<int, int> min, Tuple<int, int> max, Color color, string type)
+        {
+            Id = null;
+            Min = min;
+            Max = max;
+            Color = color;
+            Type = type;
+        }
+
+        // Override to include unique id for elements
+        public SvgElement(string id, Tuple<int, int> min, Tuple<int, int> max, Color color, string type)
+        {
+            Id = id;
+            Min = min;
+            Max = max;
+            Color = color;
+            Type = type;
+        }
+
+    }
+}
