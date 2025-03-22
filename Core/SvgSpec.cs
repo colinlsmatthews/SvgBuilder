0d787feaf90b3d5447a5f5be74052306504a9629
Colin Matthews <25379751+colinlsmatthews@users.noreply.github.com>
2025-03-22 12:08:10 -0400

    establish spec class

---
+++ Core/SvgSpec.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SvgBuilder.Core
+{
+    public class SvgSpec
+    {
+        public int Width { get; set; }
+        public int Height { get; set; }
+        public Color BackgroundColor { get; set; }
+        public List<SvgElement> Elements { get; set; }
+
+
+        public SvgSpec(int width, int height, Color backgroundColor, List<SvgElement> elements)
+        {
+            Width = width;
+            Height = height;
+            BackgroundColor = backgroundColor;
+            Elements = elements;
+        }
+
+
+    }
+}
