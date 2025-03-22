037c294eb1332f1a58c192ad282c6ca859458acf
Colin Matthews <25379751+colinlsmatthews@users.noreply.github.com>
2025-03-22 10:11:28 -0400

    Add project files.

---
+++ Console/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using SvgBuilder.Core;
+
+namespace SvgBuilder.CLI;
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Establish program variables
+        string? inputPath = String.Empty;
+        string? outputPath = String.Empty;
+        string outputDestination = String.Empty;
+        Exception e = new Exception();
+
+        // Display welcome message and prompt for input
+        Console.WriteLine("Welcome to SVG Builder!");
+        Console.WriteLine("Please enter the path to the JSON specification for your SVG:");
+        inputPath = GetInput();
+        Console.WriteLine("Please enter the path to your destination directory:");
+        outputPath = GetInput();
+        
+        // Call svg builder and display results
+        Builder svgBuilder = new Builder();
+        if (svgBuilder.Build(inputPath, outputPath, out outputDestination, out e))
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("\nSVG built successfully!");
+            Console.WriteLine($"SVG built at {outputDestination}");
+            Console.ResetColor();
+        }
+        else
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine($"\nThere was an error building your SVG:");
+            Console.WriteLine(e.Message + "\n");
+            Console.ResetColor();
+        }
+    }
+
+    private static string? GetInput()
+    {
+        string? input = Console.ReadLine();
+        if (string.IsNullOrEmpty(input))
+        {
+            Console.WriteLine("Invalid input. Please provide a valid path:");
+            GetInput();
+        }
+        return input;
+    }
+}
---
+++ Core/Builder.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace SvgBuilder.Core
+{
+    public class Builder
+    {
+        public Builder()
+        {
+        }
+
+        public bool Build(string? inputPath, string? outputPath, out string destination, out Exception e)
+        {
+            destination = @"test\path.svg";
+            e = new Exception();
+
+            try
+            {
+                if (!validatePath(inputPath))
+                {
+                    throw new Exception("Invalid input path.");
+                }
+                // TODO: add logic to read json from inputPath
+                // TODO: add logic to build svg from json
+                // TODO: add logic to save svg to outputPath
+
+            }
+            catch (Exception ex)
+            {
+                e = ex;
+                return false;
+            }
+            return true;
+        }
+
+        
+
+        private bool validatePath(string? path)
+        {
+            return true;
+        }
+
+        private string ReadJson(string inputPath)
+        {
+            JsonTextReader reader = new JsonTextReader(new StreamReader(inputPath));
+        }
+
+        private string BuildSvg(string json)
+        {
+            return String.Empty;
+        }
+
+        private bool SaveSvg(string svg, string outputPath)
+        {
+            return true;
+        }
+    }
